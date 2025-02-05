719d49491cb11154574bf4effb50da824d4e4aab
Jaime Dantas <jaimesilvajk@gmail.com>
2025-02-05 14:02:46 -0300

    recipes

--- projeto/urls.py
+++ projeto/urls.py
@@ -15,25 +15,14 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
-from django.http import HttpResponse
-from recipes.views import home
+from django.urls import path, include
 
 
 
 
-def _home(request):
-    return HttpResponse('')
-
-def sobre(request):
-    return HttpResponse('SOBRE')
-
-def contato(request):
-    return HttpResponse('CONTATO')
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', home),     
-    path('sobre/', sobre),
-    path('contato/', contato),
+    path('', include('recipes.urls')),     
+    
 ]
--- recipes/views.py
+++ recipes/views.py
@@ -1,8 +1,15 @@
-from django.shortcuts import render
 from django.http import HttpResponse
+from django.shortcuts import render
 
 
-# Create your views here.
+
 def home(request):
-    return HttpResponse('HOME')
+    return render(request, 'recipes/home.html')
+
+
+def contato(request):
+    return HttpResponse('CONTATO')
+
+def sobre(request):
+    return HttpResponse('SOBRE')
 
